673a235a51790beec6e78edccb5dce33a248b6b6
HỒ NGỌC TÙNG <91862526+shinee135@users.noreply.github.com>
2024-05-17 14:14:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(cors({
-  origin: ['http://localhost:5000'],
+  origin: ['http://localhost:5000','https://cake-store-lvhd.onrender.com'],
   credentials: true
 }));
 
